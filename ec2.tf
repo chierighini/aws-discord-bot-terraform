15e4f2f5b27cee7abd0dc8627264b0ba3e8deae1
chierighini <chierighini.andre@gmail.com>
2022-12-05 19:09:26 -0300

    change instance type

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "bot_instance" {
   ami                         = var.bot_instance_ami
-  instance_type               = "t4g.nano"
+  instance_type               = "t3.nano"
   vpc_security_group_ids      = ["${aws_security_group.sg_discord_bot.id}"]
   subnet_id                   = var.subnet_id
   associate_public_ip_address = true
