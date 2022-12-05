c0599455c8c70469e05c0834aeb35a9a5e2eddf3
chierighini <chierighini.andre@gmail.com>
2022-12-05 19:42:36 -0300

    Fix template file

--- ec2.tf
+++ ec2.tf
@@ -5,26 +5,10 @@ resource "aws_instance" "bot_instance" {
   subnet_id                   = var.subnet_id
   associate_public_ip_address = true
   key_name                    = var.ssh_key_name
-  user_data                   = <<-EOT
-    #!/bin/bash
-
-    echo "Install Dependencies"
-    yum -y update
-    yum install -y git
-    yum install -y docker
-    yum install -y yum-utils
-
-    echo "Enable systemd services"
-    systemctl enable jenkins
-    systemctl start jenkins
-    systemctl enable docker
-    systemctl start docker
-
-    echo "Run bot"
-    docker pull renatofernandes/discord-bot
-
-    docker run -e CLIENT_ID='${var.discord_client_id}' -e TOKEN='${var.discord_token}' renatofernandes/discord-bot
-  EOT
+  user_data                   = templatefile("${path.module}/run_bot.sh", {
+    discord_client_id = var.discord_client_id,
+    discord_token = var.discord_token
+  })
 
   ebs_block_device {
     device_name = "/dev/xvdb"
