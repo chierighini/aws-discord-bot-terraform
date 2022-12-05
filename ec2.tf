0369f40541f2d9947f6fc96e9f79f1dcefff83a5
chierighini <chierighini.andre@gmail.com>
2022-12-05 18:53:48 -0300

    change user_data to heredoc

--- ec2.tf
+++ ec2.tf
@@ -5,7 +5,26 @@ resource "aws_instance" "bot_instance" {
   subnet_id                   = var.subnet_id
   associate_public_ip_address = true
   key_name                    = var.ssh_key_name
-  user_data                   = file("run_bot.sh")
+  user_data                   = <<-EOT
+    #!/bin/bash
+
+    echo "Install Dependencies"
+    yum -y update
+    yum install -y git
+    yum install -y docker
+    yum install -y yum-utils
+
+    echo "Enable systemd services"
+    systemctl enable jenkins
+    systemctl start jenkins
+    systemctl enable docker
+    systemctl start docker
+
+    echo "Run bot"
+    docker pull renatofernandes/discord-bot
+
+    docker run -e CLIENT_ID='${discord_client_id}' -e TOKEN='${discord_token}' renatofernandes/discord-bot
+  EOT
 
   tags = {
     Name = "Discord bot instance"
